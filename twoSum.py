a867d7d207bb74c1c67207888b349dbf49f65bce
Aditya Gune <adi.vtca@gmail.com>
2017-01-08 15:38:09 -0800

    final commit, solution accepted

--- twoSum.py
+++ twoSum.py
@@ -1,17 +1,21 @@
-def twoSum(nums, target):
+class Solution(object):
+    def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
+        decoratedNums = []
+        b = 0
+        for a in nums:
+            decoratedNums.append((a, b))
+            b += 1
+        
         result = []
-        for i in nums:
-            for j in nums:
-                
-                if i + j == target and nums.index(i) != nums.index(j):
-                    print i, "+", j, "=", i+j
-                    result.extend((nums.index(i), nums.index(j)))
-                    print result
-
-
-twoSum([0,4,3,0], 0)
+        for i in decoratedNums:
+            for j in decoratedNums:
+                if (i[0] + j[0] == target):
+                    if (i[1] != j[1]):
+                        result.append(i[1])
+                        result.append(j[1])
+                        return result
